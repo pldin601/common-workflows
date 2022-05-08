dbd6ebed94cf37131cb1d7ae6cddc41195df5e14
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2022-05-08 19:07:08 +0100

    Use `dpkg --print-architecture` and remove `dpkg-dev` dependency

--- terraform/build-multiarch-on-aws-spots/spot.tf
+++ terraform/build-multiarch-on-aws-spots/spot.tf
@@ -81,7 +81,7 @@ resource "aws_spot_instance_request" "spot" {
   provisioner "remote-exec" {
     inline = [
       // Initialize and install docker
-      "sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-release git dpkg-dev",
+      "sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg lsb-release git",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
       "echo deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
       "sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
